51ceacd4ace3284f4d4d0c4a29931f51ec2bffe5
EGBE EYONG Michael <meyong@florence-consulting.it>
2021-11-28 21:39:04 +0100

    initial commit

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,36 @@
+package com.microservices.twitter.to.kafka.service;
+
+import com.microservices.twitter.to.kafka.service.init.StreamInitializer;
+import com.microservices.twitter.to.kafka.service.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.microservices")
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final StreamInitializer streamInitializer;
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(StreamInitializer streamInitializer, StreamRunner runner) {
+        this.streamInitializer = streamInitializer;
+        this.streamRunner = runner;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class,args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Application Starts");
+        streamInitializer.init();
+        streamRunner.start();
+    }
+}
